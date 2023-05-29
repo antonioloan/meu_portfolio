b2281dff5f95beb56dc4f2b28bea90bb749f5f1e
antonioloan <113461910+antonioloan@users.noreply.github.com>
2023-05-29 11:38:04 +0100

    arquivos do meu portfólio

---
+++ script.js
@@ -0,0 +1,27 @@
+function clicar(){
+
+    let modal = document.querySelector('.modal');
+
+    modal.style.display = 'block';
+}
+
+function fechar(){
+
+    let modal = document.querySelector('.modal');
+
+    modal.style.display = 'none';
+}
+
+function inserirmouse(){
+    let modal = document.querySelector('.foto-portfolio-voltar');
+
+    modal.style.display = 'block';
+    
+}
+  
+function tirarmouse(){
+    let modal = document.querySelector('.foto-portfolio-voltar');
+
+    modal.style.display = 'none';
+}
+
